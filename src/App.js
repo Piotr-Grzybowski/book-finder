4a0ef3a51e4d861eea6e9ff80e44e20fa1a93369
Piotr Grzybowski <grzybek6@gmail.com>
2019-03-03 12:07:21 +0000

    Better now than never. My first commit on this project

--- src/App.js
+++ src/App.js
@@ -1,28 +1,115 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import axios from 'axios';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+class BooksList extends React.Component {
+    state = {
+        booksList: ['zyTCAlFPjgYC'],
+        searchTerm: ''
+    };
+
+    search = event => {
+        event.preventDefault();
+        axios
+            .get(
+                `https://www.googleapis.com/books/v1/volumes?q=${this.state.searchTerm}&key=AIzaSyAmt5YReWJ39vXhNVGufgGKxkVfxHg0Z_A`
+            )
+            .then(res => res.data)
+            .then(res => {
+                console.log(res.items);
+                if (!res.items) {
+                this.setState({ booksList: [] });
+                return;
+            }
+
+                const booksList = res.items.map(book => book.id);
+                this.setState({
+                    booksList
+                });
+            });
+    };
+
+    handleChange = event => {
+        this.setState({
+            searchTerm: event.target.value
+        });
+    };
+
+    render() {
+        const { booksList } = this.state;
+
+        return (
+            <div>
+                <form onSubmit={this.search}>
+                    <input
+                        placeholder="Search for a book"
+                        onChange={this.handleChange}
+                    />
+                    <button type="submit">
+                        <i className="fa fa-search" />
+                    </button>
+                </form>
+                {booksList.length > 0 ? (
+                    booksList.map(book => (
+                        <BookCard bookID={book} key={book} />
+                    ))
+                ) : (
+                    <p>
+                        Couldn't find any book. Please search again using
+                        another search criteria.
+                    </p>
+                )}
+            </div>
+        );
+    }
 }
 
-export default App;
+class BookCard extends React.Component {
+    state = {
+        bookData: {}
+    };
+
+    componentDidMount() {
+        axios
+            .get(
+                `https://www.googleapis.com/books/v1/volumes/${this.props.bookID}?key=AIzaSyAmt5YReWJ39vXhNVGufgGKxkVfxHg0Z_A`
+            )
+            .then(res => res.data)
+            .then(res => {
+                this.setState({ bookData: res.volumeInfo });
+            });
+    }
+
+    render() {
+        const {
+            title,
+            authors,
+            publisher,
+            imageLinks
+        } = this.state.bookData;
+
+
+        return (
+            <div className="book-card-container">
+              <h4>Początek</h4>
+                <div className="image-container">
+                    <div
+                        className="bg-image"
+                    />
+                </div>
+                <div className="book-info">
+                    <h2>Book Details</h2>
+                    <div>
+                        <h1>{title}</h1>
+                        {console.log(this.state.bookData)}
+                        <small>Authors: {authors}</small>
+                    </div>
+                    <h4>Published by: {publisher}</h4>
+
+                </div>
+              <h4>koniec</h4>
+            </div>
+        )
+    }
+}
+export default BooksList;
